ac66eef62a9071c6481c9cd37c44b8ea8966a8ed
Jaymin Prajapati <jaymincprajapati@gmail.com>
2021-08-29 09:33:53 +0530

    Dynamic Array created

---
+++ DynamicArray/src/org/jaypraj/dynamicarray/Array.java
@@ -0,0 +1,141 @@
+package org.jaypraj.dynamicarray;
+
+import java.util.Iterator;
+
+@SuppressWarnings("unchecked")
+public class Array<T> implements Iterable<T> {
+    private T[] arr;
+    private int len = 0;
+    private int capacity = 0;
+
+    public Array() {
+        this(16);
+    }
+
+    public Array(int capacity) {
+        if (capacity < 0) throw new IllegalArgumentException("Illegal Capacity: " + capacity);
+        this.capacity = capacity;
+        arr = (T[]) new Object[capacity];
+    }
+
+    public int size() {
+        return len;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= len) throw new ArrayIndexOutOfBoundsException("Invalid index: " + index);
+        return arr[index];
+    }
+
+    public void set(int index, T element) {
+        if (index < 0 || index >= len) throw new ArrayIndexOutOfBoundsException("Invalid index: " + index);
+        arr[index] = element;
+    }
+
+    public void clear() {
+        for (int i = 0; i < capacity; i++) {
+            arr[i] = null;
+        }
+        len = 0;
+    }
+
+    public void add(T element) {
+        if (len + 1 >= capacity) {
+            increaseCapacity();
+        }
+
+        arr[len++] = element;
+    }
+
+    public void add(T[] elements) {
+        if (len + 1 >= capacity || elements.length + len >= 2 * capacity) {
+            increaseCapacity();
+        }
+
+        for (int i = 0, j = len; i < elements.length; i++, j++) {
+            arr[j] = elements[i];
+        }
+
+        len += elements.length;
+    }
+
+    private void increaseCapacity() {
+        if (capacity == 0) capacity = 1;
+        else capacity *= 2;
+        T[] newArray = (T[]) new Object[capacity];
+        for (int i = 0; i < len; i++) {
+            newArray[i] = arr[i];
+        }
+        arr = newArray;
+    }
+
+    public T removeAt(int index) {
+        if (index >= len || index < 0) throw new ArrayIndexOutOfBoundsException("Invalid index: " + index);
+        T data = arr[index];
+        T[] newArray = (T[]) new Object[len - 1];
+        for (int i = 0, j = 0; i < len; i++, j++) {
+            if (i == index) j--;
+            else newArray[j] = arr[i];
+        }
+        arr = newArray;
+        capacity = --len;
+        return data;
+    }
+
+    public boolean remove(Object obj) {
+        for (int i = 0; i < len; i++) {
+            if (arr[i].equals(obj)) {
+                removeAt(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int indexOf(Object obj) {
+        for (int i = 0; i < len; i++) {
+            if (arr[i].equals(obj)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean contains(Object obj) {
+        return indexOf(obj) != -1;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int index = 0;
+            @Override
+            public boolean hasNext() {
+                return index < len;
+            }
+
+            @Override
+            public T next() {
+                return arr[index++];
+            }
+        };
+    }
+
+    @Override
+    public String toString() {
+        if (len == 0) {
+            return "[]";
+        } else {
+            StringBuilder sb = new StringBuilder(len).append("[");
+            for (int i = 0; i < len - 1; i++) {
+                sb.append(arr[i]).append(", ");
+            }
+            sb.append(arr[len - 1]).append("]");
+            return sb.toString();
+        }
+    }
+}
---
+++ DynamicArray/src/org/jaypraj/dynamicarray/DynamicArrayTest.java
@@ -0,0 +1,43 @@
+package org.jaypraj.dynamicarray;
+
+import java.nio.channels.InterruptibleChannel;
+import java.util.Iterator;
+
+public class DynamicArrayTest {
+    public static void main(String[] args) {
+        Array<Integer> arr = new Array<>();
+
+        System.out.println(arr);
+        System.out.println(arr.isEmpty());
+        System.out.println(arr.size());
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+        System.out.println(arr.isEmpty());
+        System.out.println(arr.size());
+        System.out.println(arr);
+        System.out.println(arr.get(1));
+        System.out.println(arr.contains(20));
+        System.out.println(arr.contains(40));
+        System.out.println(arr.remove(20));
+        System.out.println(arr);
+        arr.add(40);
+        System.out.println(arr);
+        System.out.println(arr.removeAt(2));
+        System.out.println(arr.indexOf(30));
+        Iterator iterator = arr.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+        arr.set(1, 50);
+        System.out.println(arr);
+        for (int i = 0; i < 50; i++) {
+            arr.add(i * 2);
+        }
+        System.out.println(arr.size());
+        System.out.println(arr);
+        arr.add(new Integer[] {10, 20});
+        System.out.println(arr.size());
+        System.out.println(arr);
+    }
+}
