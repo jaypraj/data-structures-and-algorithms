9bde560a154cf2b107b7708ecff95909de70e414
Jaymin Prajapati <jaymincprajapati@gmail.com>
2022-06-15 16:13:45 +0530

    Add egyptian fraction problem comments

--- src/org/jaypraj/algorithms/greedy/EgyptianFraction.java
+++ src/org/jaypraj/algorithms/greedy/EgyptianFraction.java
@@ -2,6 +2,14 @@ package org.jaypraj.algorithms.greedy;
 
 import java.util.Scanner;
 
+/**
+ * Fractions with 1 in numerator and positive integer in denominator are called
+ * unit fractions. Representing any positive fraction with the sum of integer
+ * fractions is called the Egyptian fraction.
+ *
+ * @author jaymin
+ *
+ */
 public class EgyptianFraction {
   public static void main(String[] args) {
     try (Scanner sc = new Scanner(System.in)) {
@@ -15,22 +23,26 @@ public class EgyptianFraction {
   private static void printEgyptianFraction(int nr, int dr) {
     if (nr == 0 || dr == 0) return;
 
+    // Numerator is divisible by the denominator
     if (nr % dr == 0) {
       System.out.println(nr + " / " + dr + " is not a fraction");
       return;
     }
 
+    // Denominator is divisable by the numerator
     if (dr % nr == 0) {
       System.out.print("1 / " + dr / nr);
       return;
     }
 
+    // Numerator is greater than the denominator
     if (nr > dr) {
       System.out.print(nr / dr + " + ");
       printEgyptianFraction(nr % dr, dr);
       return;
     }
 
+    // Denominator is greater than the numerator
     int ceil = dr / nr + 1;
     System.out.print("1 / " + ceil + " + ");
     printEgyptianFraction(nr * ceil - dr, dr * ceil);
